0aae7b56fa3562bf969a88d02832557e651e2e47
heyou-macbook <heyou_0423@163.com>
2020-04-04 23:11:38 +0800

    add:单调栈

---
+++ src/main/java/leetcode/code/Problems20200404.java
@@ -0,0 +1,39 @@
+package leetcode.code;
+
+import java.util.Stack;
+
+/**
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ * 数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）
+ * https://leetcode-cn.com/problems/trapping-rain-water/
+ *
+ * @author heyou(heyou_0423 @ 163.com)
+ * @date 2020/3/24 23:03
+ */
+public class Problems20200404 {
+    public int trap(int[] height) {
+        if (height == null) {
+            return 0;
+        }
+        Stack<Integer> stack = new Stack<>();
+        int ans = 0;
+        for (int i = 0; i < height.length; i++) {
+            while(!stack.isEmpty() && height[stack.peek()] < height[i]) {
+                int curIdx = stack.pop();
+                // 如果栈顶元素一直相等，那么全都pop出去，只留第一个。
+                while (!stack.isEmpty() && height[stack.peek()] == height[curIdx]) {
+                    stack.pop();
+                }
+                if (!stack.isEmpty()) {
+                    int stackTop = stack.peek();
+                    // stackTop此时指向的是此次接住的雨水的左边界的位置。右边界是当前的柱体，即i。
+                    // Math.min(height[stackTop], height[i]) 是左右柱子高度的min，减去height[curIdx]就是雨水的高度。
+                    // i - stackTop - 1 是雨水的宽度。
+                    ans += (Math.min(height[stackTop], height[i]) - height[curIdx]) * (i - stackTop - 1);
+                }
+            }
+            stack.add(i);
+        }
+        return ans;
+    }
+}
